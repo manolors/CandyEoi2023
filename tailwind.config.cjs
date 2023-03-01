687f9d301c3b4bc460a83a5a13b5d2ca65b24232
Miriam Inés Alonso Hernández <miriaminesalonsohernandez@gmail.com>
2023-03-01 23:34:05 +0100

    Cambios landing, mejorando el responsive y un poco la perfomance aunque necesita mejoras

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx,html}",
   ],
   theme: {
-    extend: {},
+    opacity: {
+      0: "0",
+      25: "0.25",
+      50: "0.5",
+      75: "0.75",
+      100: "1",
+    },
+    extend: {
+      height: {
+        80: "30rem",
+      }
+    },
   },
   plugins: [],
 };
