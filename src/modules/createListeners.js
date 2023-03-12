a29a5f99b71e3825f83d4e4a2b616b1fcda0d0ba
beeg <beegdev13@gmail.com>
2023-03-12 02:56:53 +0100

    Refactorización de código Se ha mejorado el código pasando de usar innerHtml a templates para mayor reutilizabilidad Se ha creado el módulo getData para llamar al JSON Seguramente deberíamos hacer un módulo de createCards

--- src/modules/createListeners.js
+++ src/modules/createListeners.js
@@ -2,21 +2,28 @@
 // en productList.js funcione.
 
 import { addProduct } from "./addProduct";
+let buy;
+export function createListeners(action) {
+  switch (action) {
+    case "add":
+      buy = document.querySelectorAll("#addButton"); // pon este id en cada botón que quieras que tenga addProduct
 
-export function createListeners() {
-  // aquí cogemos todos los botones por el ID Es importante que en cada página que se use un botón de añadir al carrito se le ponga
-  // este ID para que el listener se añada
+      for (let i = 0; i < buy.length; i++) { // con el for recorremos todos los botones y les añadimos el eventListener
+        buy[i].addEventListener("click", () => {
+          addProduct(buy[i].dataset.value, buy[i].dataset.image, buy[i].dataset.name, buy[i].dataset.price);
+          // llamada al módulo de añadir producto
+          // los parámetros que recibe addProduct son las etiquetas que hemos puesto en los botones, y así directamente
+          // ya mete la info al hacer clic en cada producto.
+        });
+      }
+      break;
 
-  const buy = document.querySelectorAll("#addButton");
+    case "decrease":
+      // aqui iría el añadir evento de restar un producto, NO hacer la función aquí, debe ser un módulo y ser importado
+      break;
 
-  for (let i = 0; i < buy.length; i++) { // con el for recorremos todos los botones y les añadimos el eventListener
-    buy[i].addEventListener("click", () => {
-      addProduct(buy[i].dataset.value, buy[i].dataset.image, buy[i].dataset.name, buy[i].dataset.price);
-
-      // llamada al módulo de añadir producto, es importante que en cada página que se utilice un boton de añadir al carrito
-      // se importe la función addProduct.
-      // los parámetros que recibe addProduct son las etiquetas que hemos puesto en los botones, y así directamente
-      // ya mete la info al hacer clic en cada producto.
-    });
+    case "delete":
+      // aquí iría el añadir evento de eliminar producto, NO hacer la función aquí, debe ser un módulo y ser importado
+      break;
   }
 }
