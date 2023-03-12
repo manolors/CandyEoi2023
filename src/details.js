f79a692e4f01a8df211a8d6c3afcfe7fbd68b0ce
María Mira <maria.alaves93@gmail.com>
2023-03-12 19:18:26 +0100

    Añadido función random para que cada vez salga un producto distinto en productos recomendados

--- src/details.js
+++ src/details.js
@@ -83,15 +83,15 @@ const $recomended = document.querySelector("#recomended");
 async function getDataRecommended() {
   const res = await fetch("./assets/products.json");
   const json = await res.json();
-  console.log(json);
-  for (let i = 0; i < 4; i++) {
+  const chosenProducts = random(0, json.length, 4);
+  for (let i = 0; i < chosenProducts.length; i++) {
     const $div = document.createElement("div");
     $div.innerHTML = `<div class="flex flex-col md:flex-col lg:flex-row lg:gap-9 md:gap-3 gap-3 md:w-48 md:h-48 lg:w-full lg:h-72">
                         <div class="flex flex-row md:flex-col lg:flex-col gap-2 items-center border-1 lg:text-center border-gray-100 rounded-lg shadow-lg lg:shadow-xl lg:w-60 lg:h-72 lg:hover:scale-110 md:hover:scale-110 md:hover:ease-linear duration-150 ease-in">
-                        <img class="w-20 h-20 md:w-full lg:w-full md:h-full lg:h-48 rounded-l-lg md:rounded-t-lg md:rounded-bl-none lg:rounded-t-lg lg:rounded-bl-none" src="${json[i].image}" alt="${json[i].name}"></a>
+                        <img class="w-20 h-20 md:w-full lg:w-full md:h-full lg:h-48 rounded-l-lg md:rounded-t-lg md:rounded-bl-none lg:rounded-t-lg lg:rounded-bl-none" src="${json[chosenProducts[i]].image}" alt="${json[chosenProducts[i]].name}"></a>
                         <div class="flex flex-col lg:p-4 gap-2">
-                        <h1><a class="font-bold text-left p-3" href="detalle.html?id=${json[i].id}">${json[i].name}</a></h1>
-                        <h3 class="font-semibold">${json[i].price}€</h3>
+                        <h1><a class="font-bold text-left p-3" href="detalle.html?id=${json[chosenProducts[i]].id}">${json[chosenProducts[i]].name}</a></h1>
+                        <h3 class="font-semibold">${json[chosenProducts[i]].price}€</h3>
                         </div>
                         </div>
                         </div>`;
@@ -99,3 +99,25 @@ async function getDataRecommended() {
   };
 }
 getDataRecommended();
+
+function random(min, max, num) {
+  const res = [];
+  for (let i = 0; i < num; i++) {
+    // mientras la longitud de "res" (que tiene los valores a devolver) es igual al índice del for (quiere decir que no se ha añadido a res nada nuevo)
+    while (res.length == i) {
+      let found = false;
+      const number = Math.floor(Math.random() * max) + min;
+      for (const value of res) {
+        if (value == number) {
+          found = true;
+        }
+      }
+
+      if (!found) {
+        res.push(number);
+      }
+    }
+  }
+
+  return res;
+}
